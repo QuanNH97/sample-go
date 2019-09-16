061a94d17d053738975a14a89408f0bcea7eae2c
quannh <quannh220497@gmail.com>
2019-09-16 13:51:12 +0700

    add feature login

--- app/router/router.go
+++ app/router/router.go
@@ -5,6 +5,8 @@ import (
 
 	"sample/app/hello"
 	"sample/app/infrastructure"
+	"sample/app/login"
+	"sample/app/logout"
 	"sample/app/shared/handler"
 
 	"github.com/go-chi/chi"
@@ -55,8 +57,14 @@ func (r *Router) SetupHandler() {
 	// uh := user.NewHTTPHandler(br, bu, bh, r.SQLHandler, r.CacheHandler)
 
 	hw := hello.NewHTTPHandler(ah)
+	lg := login.NewLoginHTTPHandler(ah)
+	lo := logout.NewLogoutHTTPHandler(ah)
 
 	r.Mux.Route("/", func(cr chi.Router) {
 		cr.Get("/hello", hw.HelloWorld)
+		cr.Get("/login", lg.Login)
+		cr.Post("/login", lg.HandleLogin)
+		cr.Get("/logout", lo.Logout)
+		cr.Post("/logout", lo.HandlerLogout)
 	})
 }
